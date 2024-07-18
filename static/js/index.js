006ffc6c0d815e2cd38f56d62b7ca2caa673b7e6
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2024-07-18 17:49:21 -0300

    fiz algumas alterações no design do site

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
+/* menu */
+
 const butao_carrino = document.querySelector('#butao-carrinho')
 const itens_carrinho = document.querySelector('#itens-carrinho')
 
@@ -8,3 +10,30 @@ butao_carrino.addEventListener('click', () => {
         itens_carrinho.classList.add('carrinho-ativado')
     }
 })
+
+/* produtos */
+
+const produtos = document.querySelectorAll('.nome-produto')
+produtos.forEach(element => {
+    const nome = element.innerText
+
+    const mais = document.getElementById(nome+'-mais');
+    const menos = document.getElementById(nome+'-menos');
+    const quantidade = document.getElementById(nome+'-quant');
+    const adicionar = document.getElementById(nome+'-adicionar');
+    console.log(nome)
+    console.log(mais)
+    mais.addEventListener('click', () => {
+        const q = +quantidade.innerText + 1
+        quantidade.innerHTML = q
+    })
+    
+    menos.addEventListener('click', () => {
+        if (+quantidade.innerText > 1) {
+            const q = +quantidade.innerText - 1
+            quantidade.innerHTML = q
+        }
+    })
+});
+
+/* produtos */
